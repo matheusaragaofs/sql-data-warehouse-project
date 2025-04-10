0282779c8e55aceea203abec6aaf22d2eb0c13cc
matheus <matheusaragaofs@gmail.com>
2025-04-10 13:02:32 -0300

    Refactor load_silver procedure to enhance data loading and cleaning process for customer data

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -3,15 +3,26 @@
 Stored Procedure or Script: Load and Clean Customer Data (Bronze -> Silver)
 ===============================================================================
 Script Purpose:
-    This script loads cleaned and standardized customer data from the bronze layer 
-    into the silver layer (table: silver.crm_cust_info). It ensures data consistency 
-    and quality through the following steps:
-    
-    - Trims unwanted spaces from string fields (first name, last name, etc.)
-    - Standardizes coded values such as gender and marital status
-    - Handles missing or unrecognized values by assigning a default ('n/a')
-    - Removes duplicates by selecting the most recent record per customer ID
-    - Ensures only one record per customer is inserted
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+  
+    crm_customer_info
+        - Trims unwanted spaces from string fields (first name, last name, etc.)
+        - Standardizes coded values such as gender and marital status
+        - Handles missing or unrecognized values by assigning a default ('n/a')
+        - Removes duplicates by selecting the most recent record per customer ID
+        - Ensures only one record per customer is inserted
+    crm_prd_info
+        - 
+        -
+    crm_sales_details
+        -
+        -
+    erp_loc_a101
+        -
+        -
+
+
 
 Parameters:
     None.
@@ -22,37 +33,69 @@ Usage Example:
 ===============================================================================
 */
 
-INSERT INTO silver.crm_cust_info (
-cst_id,
-cst_key,
-cst_firstname,
-cst_lastname,
-cst_marital_status,
-cst_gndr,
-cst_create_date
-) 
-SELECT 
-cst_id,
-cst_key,
-TRIM(cst_firstname) AS cst_firstname,
-TRIM(cst_lastname) AS cst_lastname,
-CASE 
-	WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-	WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
-	ELSE 'n/a'
-END cst_marital_status,
-CASE 
-	WHEN UPPER(TRIM(cst_gndr)) ='F' then 'Female'
-	WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-	ELSE 'n/a' -- not available
-END AS cst_gndr,
-cst_create_date
-FROM (
-	SELECT 
-	*,
-	ROW_NUMBER() OVER (
-	PARTITION BY cst_id
-	ORDER BY cst_create_date DESC) as flag_last
-	FROM bronze.crm_cust_info
-	WHERE cst_id IS NOT NULL
-) t WHERE flag_last = 1;
+CREATE OR ALTER PROCEDURE silver.load_silver AS
+BEGIN
+    DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
+    BEGIN TRY
+        SET @batch_start_time = GETDATE();
+        PRINT '================================================';
+        PRINT 'Loading Silver Layer';
+        PRINT '================================================';
+
+		PRINT '------------------------------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '------------------------------------------------';
+
+		-- Loading silver.crm_cust_info
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.crm_cust_info';
+		TRUNCATE TABLE silver.crm_cust_info;
+		PRINT '>> Inserting Data Into: silver.crm_cust_info';
+		INSERT INTO silver.crm_cust_info (
+			cst_id, 
+			cst_key, 
+			cst_firstname, 
+			cst_lastname, 
+			cst_marital_status, 
+			cst_gndr,
+			cst_create_date
+		)
+		SELECT
+			cst_id,
+			cst_key,
+			TRIM(cst_firstname) AS cst_firstname,
+			TRIM(cst_lastname) AS cst_lastname,
+			CASE 
+				WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+				WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+				ELSE 'n/a'
+			END AS cst_marital_status, -- Normalize marital status values to readable format
+			CASE 
+				WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+				ELSE 'n/a'
+			END AS cst_gndr, -- Normalize gender values to readable format
+			cst_create_date
+		FROM (
+			SELECT
+				*,
+				ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+			FROM bronze.crm_cust_info
+			WHERE cst_id IS NOT NULL
+		) t
+		WHERE flag_last = 1; -- Select the most recent record per customer
+		SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> -------------';
+  
+		
+	END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
+END
